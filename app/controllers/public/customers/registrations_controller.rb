a07e65854225c32606946e1eba117876feb38ef0
yuuki-1227 <uncle.ozin.ochiai@gmail.com>
2021-05-17 09:14:06 +0000

    [Update]customer側 新規登録画面,ログイン画面

--- app/controllers/public/customers/registrations_controller.rb
+++ app/controllers/public/customers/registrations_controller.rb
@@ -4,6 +4,22 @@ class Public::Customers::RegistrationsController < Devise::RegistrationsControll
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def after_sign_up_path_for(resource)
+    products_path
+  end
+
+  def after_sign_in_path_for(resource)
+    products_path
+  end
+
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :kana_last_name, :kana_first_name, :is_deleted, :postal_code, :residence, :phone_number])
+  end
   # GET /resource/sign_up
   # def new
   #   super
