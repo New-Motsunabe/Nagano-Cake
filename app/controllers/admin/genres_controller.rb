f9ecff67c70c37204d9be4150afa666f46bf4066
kajisan0415 <agonagakajisan0415@gmail.com>
2021-05-17 03:11:57 +0000

    編集機能作成

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,2 +1,39 @@
 class Admin::GenresController < ApplicationController
+
+  def index
+    @genres = Genre.all
+    @genre = Genre.new
+  end
+
+  def create
+    @genre = Genre.new(genre_params)
+    if @genre.save
+      flash[:notice] = "登録されました"
+      redirect_to admin_genres_path
+    else
+      @genres = Genres.all
+      render :index and return
+    end
+  end
+
+  def edit
+    @genre = Genre.find(params[:id])
+  end
+
+  def update
+        @genre = Genre.find(params[:id])
+    if @genre.update(genre_params)
+      flash[:success] = "ジャンルを変更しました"
+      redirect_to admin_genres_path
+    else
+      render :edit and return
+    end
+  end
+
+
+  private
+
+  def genre_params
+    params.require(:genre).permit(:genre_name)
+  end
 end
