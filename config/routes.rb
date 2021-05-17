ac8d0ad90bc3f2b147a235afa318cb93145a5d25
kajisan0415 <agonagakajisan0415@gmail.com>
2021-05-17 09:17:40 +0000

    トップページ写真追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  
+
   devise_for :admins
   devise_for :customers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
@@ -20,18 +20,18 @@ Rails.application.routes.draw do
   resources :shipping_addresses
   resources :genres
   resources :ordered_products
-  
+
   get "customers/unsubscribe" => "public/customers#unsubscribe"
   patch "customers/withdraw" => "public/customers#withdraw"
-    
+
   delete "cart_items/destroy_all" => "public/cart_items#destroy_all"
-    
+
   post "orders/confirm" => "public/orders#confirm"
   get "orders/complete" => "public/orders#complete"
-  
+
   get "customers/my_page" => "public/customers#show"
-  
-  root to: "homes#top"
+
+  root to: "public/homes#top"
   get "/about" => "public/homes#about"
- 
+
 end
