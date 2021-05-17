7fed68323e7daf06c4098c39bc9724998ae6bef8
Takada-Aoi <a.m.3m0221@gmail.com>
2021-05-17 05:53:54 +0000

    調整

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,2 +1,12 @@
 class Admin::HomesController < ApplicationController
+  
+  def top
+    case params[:order_sort]
+      when "1"
+       @user = User.find(params[:user_id])
+       @orders = @user.orders
+    else
+       @orders = Order.all
+    end
+  end
 end
