c63a9688d3ce7b154143928152ac729bf458e7b7
yuuki-1227 <uncle.ozin.ochiai@gmail.com>
2021-05-18 04:37:24 +0000

    [Update]退会機能

--- app/controllers/public/customers/sessions_controller.rb
+++ app/controllers/public/customers/sessions_controller.rb
@@ -17,7 +17,8 @@ class Public::Customers::SessionsController < Devise::SessionsController
   def reject_inactive_customer
     @customer = Customer.find_by(email: params[:customer][:email])
     if @customer
-      if @customer.valid_password?(params[:customer][:password]) && (@customer.is_deleted == false)
+      if @customer.valid_password?(params[:customer][:password]) && (@customer.is_deleted == true)
+        flash[:notice] = "退会済みです。再度ご登録をしてご利用ください"
         redirect_to new_customer_registration_path
       end
     end
