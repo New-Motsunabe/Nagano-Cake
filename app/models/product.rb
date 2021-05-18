6120c5a921cf7eef026f7ef1ccf1d00e7a9b6722
Takada-Aoi <a.m.3m0221@gmail.com>
2021-05-18 13:50:26 +0000

    [追加]

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,16 +2,12 @@ class Product < ApplicationRecord
   has_many :genres, dependent: :destroy
   has_many :ordered_products, dependent: :destroy
 
-<<<<<<< HEAD
-  attachment :image, destroy: false
-=======
    attachment :image, destroy: false
-   
+
   validates :genre_id, presence: true
   validates :name, presence: true
   validates :introduction, presence: true
   validates :price, presence: true
   validates :is_active, presence: true
   validates :image, presence: true
->>>>>>> origin/develop
 end
