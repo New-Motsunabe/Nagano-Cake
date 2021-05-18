396d7b93f929c5143d1bac5569e05e73acee8f40
Ltlyl-tk79 <taku.805740@gmail.com>
2021-05-18 11:00:03 +0000

    first commit

--- app/controllers/public/products_controller.rb
+++ app/controllers/public/products_controller.rb
@@ -1,2 +1,6 @@
 class Public::ProductsController < ApplicationController
+  def index
+    @products = Product.where(is_active :ture).page(params[:page]).per(8)
+    @quantity = Product.
+  end
 end
