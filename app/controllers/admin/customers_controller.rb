a3bfc0ef52c83cf4039eb176a375bff9ddc81031
yuuki-1227 <uncle.ozin.ochiai@gmail.com>
2021-05-18 07:39:14 +0000

    [Add]会員一覧、会員編集画面、会員詳細画面

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -8,4 +8,21 @@ class Admin::CustomersController < ApplicationController
     @customer = Customer.find(params[:id])
   end
 
+  def edit
+        @customer = Customer.find(params[:id])
+  end
+
+  def update
+    @customer = Customer.find(params[:id])
+    @customer.update(customer_params)
+    redirect_to admin_customer_path(@customer)
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :kana_last_name, :kana_first_name, :is_deleted, :postal_code, :residence, :phone_number)
+  end
+
+
 end
