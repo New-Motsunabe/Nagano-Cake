a61a2a10184de685c73c87f2b0271feed45e669f
Takada-Aoi <a.m.3m0221@gmail.com>
2021-05-22 04:04:20 +0000

    merge前

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -2,15 +2,14 @@ class Admin::HomesController < ApplicationController
   before_action :authenticate_admin!
 
  def top
-    
-       @customer = params[:customer_id]
-       if @customer
-        @orders = Order.where(customer_id: @customer)
+  @customer = params[:customer_id]
+    if @customer
+       @orders = Order.where(customer_id: @customer)
        #@amount = @orders.ordered_products.sum
-       else
-        @orders = Order.all
+    else
+       @orders = Order.all
        #@amount = @orders.ordered_products.sum
-        end
+    end
  end
 
 end
