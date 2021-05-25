711bf7d21b208da9b7b721aefb84800661711319
kajisan0415 <agonagakajisan0415@gmail.com>
2021-05-25 05:10:10 +0000

    調整

--- app/controllers/admin/ordered_products_controller.rb
+++ app/controllers/admin/ordered_products_controller.rb
@@ -4,7 +4,7 @@ class Admin::OrderedProductsController < ApplicationController
   def update
     ordered_product = OrderedProduct.find(params[:id])
     ordered_product.update(ordered_product_params)
-    ordered_product.change_order_status　#change_order_statusメソッドはordered_productモデルに定義してます。
+    ordered_product.change_order_status#change_order_statusメソッドはordered_productモデルに定義してます。
     redirect_back(fallback_location: root_path)
   end
 
